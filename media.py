cce801cfb6d3085ee79a185bcec1c5b3cf8a8b33
aragorn-wu <wuqingfeng234@163.com>
2018-04-27 17:54:25 +0800

    更新菜单和永久素材的代码完成，但是微信公众号没有权限

--- media.py
+++ media.py
@@ -1,28 +1,58 @@
 # -*- coding: utf-8 -*-
-# filename: media.py
-from basic import Basic
+# filename: material.py
 import urllib2
+import json
 import poster.encode
 from poster.streaminghttp import register_openers
+from basic import Basic
 
-class Media(object):
+class Material(object):
     def __init__(self):
         register_openers()
-    #上传图片
+    #上传
     def uplaod(self, accessToken, filePath, mediaType):
         openFile = open(filePath, "rb")
-        param = {'media': openFile}
+        fileName = "hello"
+        param = {'media': openFile, 'filename': fileName}
+        #param = {'media': openFile}
         postData, postHeaders = poster.encode.multipart_encode(param)
 
-        postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s" % (accessToken, mediaType)
+        postUrl = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=%s&type=%s" % (accessToken, mediaType)
         request = urllib2.Request(postUrl, postData, postHeaders)
         urlResp = urllib2.urlopen(request)
         print urlResp.read()
+    #下载
+    def get(self, accessToken, mediaId):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/material/get_material?access_token=%s" % accessToken
+        postData = "{ \"media_id\": \"%s\" }" % mediaId
+        urlResp = urllib2.urlopen(postUrl, postData)
+        headers = urlResp.info().__dict__['headers']
+        if ('Content-Type: application/json\r\n' in headers) or ('Content-Type: text/plain\r\n' in headers):
+            jsonDict = json.loads(urlResp.read())
+            print jsonDict
+        else:
+            buffer = urlResp.read()  # 素材的二进制
+            mediaFile = file("test_media.jpg", "wb")
+            mediaFile.write(buffer)
+            print "get successful"
+    #删除
+    def delete(self, accessToken, mediaId):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/material/del_material?access_token=%s" % accessToken
+        postData = "{ \"media_id\": \"%s\" }" % mediaId
+        urlResp = urllib2.urlopen(postUrl, postData)
+        print urlResp.read()
+
+    #获取素材列表
+    def batch_get(self, accessToken, mediaType, offset=0, count=20):
+        postUrl = ("https://api.weixin.qq.com/cgi-bin/material"
+               "/batchget_material?access_token=%s" % accessToken)
+        postData = ("{ \"type\": \"%s\", \"offset\": %d, \"count\": %d }"
+                    % (mediaType, offset, count))
+        urlResp = urllib2.urlopen(postUrl, postData)
+        print urlResp.read()
 
 if __name__ == '__main__':
-    myMedia = Media()
+    myMaterial = Material()
     accessToken = Basic().get_access_token()
-    filePath = "1s.jpg"   #请安实际填写
-    mediaType = "image"
-    myMedia.uplaod(accessToken, filePath, mediaType)
-
+    mediaType = "news"
+    myMaterial.batch_get(accessToken, mediaType)
