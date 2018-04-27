0d802e2899d5c5b5b7bf5fc4ad44334080636b75
aragorn-wu <wuqingfeng234@163.com>
2018-04-27 17:12:40 +0800

    接口上传临时素材

--- handle.py
+++ handle.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
 # filename: handle.py
-
 import hashlib
+import reply
+import receive
 import web
 
 class Handle(object):
-    def GET(self):
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            print "signature is: ",signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            print "echostr is: ",echostr
-            token = "hello2016" #请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print "Handle Post webdata is ", webData   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg):
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
             else:
-                print "error occurred, hashcode is not equal to sinnature"
-                return ""
-        except Exception, Argument:
-            return Argument
+                print "暂且不处理"
+                return reply.Msg().send()
+        except Exception, Argment:
+            return Argment
