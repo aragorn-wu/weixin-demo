62ce8f481b60fe6cbbf926a1855a1a9f6440ba1a
aragorn-wu <wuqingfeng234@163.com>
2018-04-27 15:15:31 +0800

    能够通过公众号验证了

---
+++ handle.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# filename: handle.py
+
+import hashlib
+import web
+
+class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            print "signature is: ",signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            print "echostr is: ",echostr
+            token = "hello2016" #请按照公众平台官网\基本配置中信息填写
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            if hashcode == signature:
+                return echostr
+            else:
+                print "error occurred, hashcode is not equal to sinnature"
+                return ""
+        except Exception, Argument:
+            return Argument
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-#-*- coding: utf-8-*-
-#filename main.py
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+from handle import Handle
 
-import  web
-
-urls=(
-    '/wx','Handle',
+urls = (
+    '/wx', 'Handle',
 )
-
-class Handle(object):
-    def GET(self):
-        return "hello, this is a test"
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
